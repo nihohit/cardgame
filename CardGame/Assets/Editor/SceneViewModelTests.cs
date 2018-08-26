4102cbd73ab116faffea364768b44a275f688dbc
Shachar <nihohit@gmail.com>
2018-08-26 22:19:04 +0300

    Added scene view model.

---
+++ CardGame/Assets/Editor/SceneViewModelTests.cs
@@ -0,0 +1,276 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+using UniRx;
+
+public class SceneViewModelTests {
+	private static Card[] initialDeck = new[] { cardWithName("foo"), cardWithName("bar"), cardWithName("baz"), cardWithName("bro") };
+
+	private static Card cardWithName(string name) {
+		return new Card {
+			Name = name
+		};
+	}
+
+	private class FakeModel : ISceneModel {
+		public Subject<SceneState> StateSubject { get; private set; } = new Subject<SceneState>();
+		public Card PlayedCard { get; private set; }
+		public bool TryDrawCardCalled { get; private set; }
+		public bool UserFinishedModeCalled { get; private set; }
+
+		public IObservable<SceneState> State => StateSubject;
+
+		public bool PlayCard(Card card) {
+			PlayedCard = card;
+			return true;
+		}
+
+		public void TryDrawCard() {
+			TryDrawCardCalled = true;
+		}
+
+		public void UserFinishedMode() {
+			UserFinishedModeCalled = true;
+		}
+	}
+
+	private FakeModel fakeModel;
+	private SceneViewModel viewModel;
+
+	[SetUp]
+	public void SetupTests() {
+		fakeModel = new FakeModel();
+		viewModel = new SceneViewModel(fakeModel);
+	}
+
+	private SceneState scene(CardsState cards = null, 
+		EmpireState empire = null, 
+		CardHandlingMode mode = CardHandlingMode.Regular,
+		EventCard currentEvent = null) {
+		return new SceneState(cards, empire, mode, currentEvent);
+	}
+
+	#region imports
+	[Test]
+  public void PassDoneButtonClicksToModel() {
+		var subject = new Subject<Unit>();
+		viewModel.setDoneButtonClicked(subject);
+
+		Assert.IsFalse(fakeModel.UserFinishedModeCalled);
+
+		subject.OnNext(Unit.Default);
+
+		Assert.IsTrue(fakeModel.UserFinishedModeCalled);
+	}
+
+	[Test]
+	public void PassDeckClicksToModelAsAttemptsToDrawCards() {
+		var subject = new Subject<Unit>();
+		viewModel.setDeckWasClicked(subject);
+
+		Assert.IsFalse(fakeModel.TryDrawCardCalled);
+
+		subject.OnNext(Unit.Default);
+
+		Assert.IsTrue(fakeModel.TryDrawCardCalled);
+	}
+
+	[Test]
+	public void PassSelectedCardModel() {
+		var subject = new Subject<Card>();
+		var card = new Card();
+		viewModel.setSelectedCardObservation(subject);
+
+		Assert.IsNull(fakeModel.PlayedCard);
+
+		subject.OnNext(card);
+
+		Assert.AreSame(card, fakeModel.PlayedCard);
+	}
+	#endregion
+
+	#region outputs
+
+	[Test]
+	public void PassStateDescription() {
+		var description = viewModel.StateDescription.ToReactiveProperty();
+
+		var state = new EmpireState(1, 2, 3, 4);
+		fakeModel.StateSubject.OnNext(scene(empire: state));
+
+		Assert.AreEqual(state.ToString(), description.Value);
+	}
+
+	[Test]
+	public void PassDeckCount() {
+		var count = viewModel.DeckCount.ToReactiveProperty();
+
+		var cards = CardsState.NewState(initialDeck);
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(0, count.Value);
+
+		cards = cards.ShuffleCurrentDeck();
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(4, count.Value);
+
+		cards = cards.DrawCardsToHand(1);
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(3, count.Value);
+	}
+
+	[Test]
+	public void PassDiscardPileCount() {
+		var count = viewModel.DiscardPileCount.ToReactiveProperty();
+
+		var cards = CardsState.NewState(initialDeck);
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(0, count.Value);
+
+		cards = cards.ShuffleCurrentDeck().DrawCardsToHand(2);
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(0, count.Value);
+
+		cards = cards.DiscardHand();
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(2, count.Value);
+	}
+
+	[Test]
+	public void PassHand() {
+		var hand = viewModel.Hand.ToReactiveProperty();
+
+		var cards = CardsState.NewState(initialDeck).ShuffleCurrentDeck();
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		CollectionAssert.AreEquivalent(new Card[0], hand.Value);
+
+		cards = cards.DrawCardsToHand(4);
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(initialDeck, hand.Value);
+
+		cards = cards.DiscardHand();
+		fakeModel.StateSubject.OnNext(scene(cards: cards));
+
+		Assert.AreEqual(new Card[0], hand.Value);
+	}
+
+	[Test]
+	public void PassDisplayDoneButtonAccordingToMode() {
+		var shouldDisplay = viewModel.DisplayDoneButton.ToReactiveProperty();
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Exhaust));
+
+		Assert.IsTrue(shouldDisplay.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Replace));
+
+		Assert.IsTrue(shouldDisplay.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Regular));
+
+		Assert.IsTrue(shouldDisplay.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Event));
+
+		Assert.IsFalse(shouldDisplay.Value);
+	}
+
+	[Test]
+	public void PassCardsInMultiDisplay() {
+		var displayedCards = viewModel.CardsInMultiDisplay.ToReactiveProperty();
+
+		var cards = CardsState.NewState(initialDeck).ShuffleCurrentDeck().DrawCardsToHand(4);
+		var eventOptions = new Card[] { cardWithName("hi"), cardWithName("bye") };
+		var eventCard = new EventCard {
+			Name = "test",
+			Options = eventOptions
+		};
+		fakeModel.StateSubject.OnNext(scene(cards: cards, 
+			mode: CardHandlingMode.Exhaust, 
+			currentEvent: eventCard));
+
+		CollectionAssert.AreEquivalent(initialDeck, displayedCards.Value);
+
+		fakeModel.StateSubject.OnNext(scene(cards: cards,
+			mode: CardHandlingMode.Event,
+			currentEvent: eventCard));
+
+		CollectionAssert.AreEquivalent(eventOptions, displayedCards.Value);
+
+		fakeModel.StateSubject.OnNext(scene(cards: cards,
+			mode: CardHandlingMode.Replace,
+			currentEvent: eventCard));
+
+		CollectionAssert.AreEquivalent(initialDeck, displayedCards.Value);
+	}
+
+	[Test]
+	public void PassMultiDisplayTextAccordingToState() {
+		var description = viewModel.TextForMultiDisplay.ToReactiveProperty();
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Exhaust));
+
+		Assert.AreEqual("Choose cards to exhaust", description.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Replace));
+
+		Assert.AreEqual("Choose cards to replace", description.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Event, currentEvent: new EventCard {
+			Name = "test"
+		}));
+
+		Assert.AreEqual(description.Value, "test");
+	}
+
+	[Test]
+	public void PassTextForDoneButton() {
+		var description = viewModel.TextForDoneButton.ToReactiveProperty();
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Exhaust));
+
+		Assert.AreEqual("Done", description.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Regular));
+
+		Assert.AreEqual("End Turn", description.Value);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Replace));
+
+		Assert.AreEqual("Done", description.Value);
+	}
+
+	[Test]
+	public void PassHideMultiDisplay() {
+		int numberOfCounts = 0;
+		viewModel.HideMultiDisplay.Subscribe(_ => {
+			++numberOfCounts;
+		});
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Exhaust));
+
+		Assert.AreEqual(0, numberOfCounts);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Replace));
+
+		Assert.AreEqual(0, numberOfCounts);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Regular));
+
+		Assert.AreEqual(1, numberOfCounts);
+
+		fakeModel.StateSubject.OnNext(scene(mode: CardHandlingMode.Event));
+
+		Assert.AreEqual(1, numberOfCounts);
+	}
+	#endregion
+}
---
+++ CardGame/Assets/scripts/View model/SceneViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public interface ISceneViewModel {
+	#region inputs
+	void setDoneButtonClicked(IObservable<Unit> observable);
+	void setSelectedCardObservation(IObservable<Card> observable);
+	void setDeckWasClicked(IObservable<Unit> observable);
+	#endregion
+
+	#region outputs
+	IObservable<string> StateDescription { get; }
+	IObservable<int> DeckCount { get; }
+	IObservable<int> DiscardPileCount { get; }
+	IObservable<IEnumerable<Card>> Hand { get; }
+	IObservable<bool> DisplayDoneButton { get; }
+	IObservable<IEnumerable<Card>> CardsInMultiDisplay { get; }
+	IObservable<string> TextForMultiDisplay { get; }
+	IObservable<string> TextForDoneButton { get; }
+	IObservable<Unit> HideMultiDisplay { get; }
+	#endregion
+}
+
+public class SceneViewModel : ISceneViewModel {
+	private ISceneModel model;
+
+	#region ISceneViewModel
+	public void setDoneButtonClicked(IObservable<Unit> observable) {
+		observable.Subscribe(_ => model.UserFinishedMode());
+	}
+
+	public void setSelectedCardObservation(IObservable<Card> observable) {
+		observable.Subscribe(cardWasClicked);
+	}
+
+	private void cardWasClicked(Card card) {
+		model.PlayCard(card);
+	}
+
+	public void setDeckWasClicked(IObservable<Unit> observable) {
+		observable.Subscribe(_ => model.TryDrawCard());
+	}
+
+	public IObservable<string> StateDescription => model.State
+		.Select(state => state.Empire.ToString());
+
+	public IObservable<int> DeckCount => model.State
+		.Select(state => state.Cards.CurrentDeck.Count());
+
+	public IObservable<int> DiscardPileCount => model.State
+		.Select(state => state.Cards.DiscardPile.Count());
+
+	public IObservable<IEnumerable<Card>> Hand => model.State
+		.Select(state => state.Cards.Hand);
+
+	public IObservable<bool> DisplayDoneButton => model.State
+		.Select(state => state.Mode != CardHandlingMode.Event);
+
+	public IObservable<IEnumerable<Card>> CardsInMultiDisplay => model.State
+		.Where(state => state.Mode != CardHandlingMode.Regular)
+		.Select(cardsInMultiCardDisplay);
+
+	private IEnumerable<Card> cardsInMultiCardDisplay(SceneState state) {
+		if (state.Mode == CardHandlingMode.Event) {
+			return state.CurrentEvent.Options;
+		}
+		return state.Cards.Hand;
+	}
+
+	public IObservable<string> TextForMultiDisplay => model.State
+		.Where(state => state.Mode != CardHandlingMode.Regular)
+		.Select(stateMultiCardDescription);
+
+	private string stateMultiCardDescription(SceneState state) {
+		switch (state.Mode) {
+			case CardHandlingMode.Exhaust:
+				return "Choose cards to exhaust";
+			case CardHandlingMode.Replace:
+				return "Choose cards to replace";
+			case CardHandlingMode.Event:
+				return state.CurrentEvent.Name;
+			default:
+				AssertUtils.UnreachableCode();
+				break;
+		}
+		return null;
+	}
+
+	public IObservable<string> TextForDoneButton => model.State
+		.Select(state => state.Mode == CardHandlingMode.Regular ? "End Turn" : "Done");
+
+	public IObservable<Unit> HideMultiDisplay => model.State
+		.Select(state => state.Mode)
+		.DistinctUntilChanged()
+		.Where(mode => mode == CardHandlingMode.Regular)
+		.Select(_ => Unit.Default);
+	#endregion
+
+	public SceneViewModel() : this(new SceneModel()) { }
+
+	public SceneViewModel(ISceneModel model) {
+		this.model = model;
+	}
+}
