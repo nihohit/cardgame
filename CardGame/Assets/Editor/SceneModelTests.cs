16259e74fa858a4f7f96dd68d1e9c989b9423862
Shachar <nihohit@gmail.com>
2018-08-26 22:17:40 +0300

    Added scene model.

---
+++ CardGame/Assets/Editor/SceneModelTests.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UniRx;
+using System.Linq;
+
+public class SceneModelTests {
+	private static Card[] initialDeck = new[] { cardWithName("foo"), cardWithName("bar"), cardWithName("baz"), cardWithName("bro") };
+
+	private static Card cardWithName(string name) {
+		return new Card {
+			Name = name
+		};
+	}
+
+	[Test]
+	public void ShouldHaveInitialStateReadyOnConstruction() {
+		var cards = CardsState.NewState(initialDeck).ShuffleCurrentDeck();
+		var empire = new EmpireState(1, 2, 3, 4);
+		var mode = CardHandlingMode.Exhaust;
+		var model = new SceneModel(cards, empire, mode);
+
+		var state = model.State.ToReactiveProperty();
+
+		Assert.AreEqual(cards, state.Value.Cards);
+		Assert.AreEqual(empire, state.Value.Empire);
+		Assert.AreEqual(mode, state.Value.Mode);
+	}
+
+	[Test]
+	public void ShouldEndTurnWhenUserFinishedModeIsCalledInRegularMode() {
+		var deck = new Card[] { new Card {
+			Name = "foo",
+			GoldCost = 1,
+			GoldGain = 5
+		}};
+		var model = new SceneModel(CardsState.NewState(deck).ShuffleCurrentDeck().DrawCardsToHand(1),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Regular);
+		var state = model.State.ToReactiveProperty();
+		model.PlayCard(deck[0]);
+
+		model.UserFinishedMode();
+
+		Assert.AreEqual(new EmpireState(5, 0, 0, 0), state.Value.Empire);
+	}
+
+	[Test]
+	public void ShouldReturnToRegularModeFromReplace() {
+		var model = new SceneModel(CardsState.NewState(initialDeck).ShuffleCurrentDeck(),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Replace);
+		var state = model.State.ToReactiveProperty();
+
+		model.UserFinishedMode();
+
+		Assert.AreEqual(CardHandlingMode.Regular, state.Value.Mode);
+	}
+
+	[Test]
+	public void ShouldReturnToRegularModeFromExhaust() {
+		var model = new SceneModel(CardsState.NewState(initialDeck).ShuffleCurrentDeck(),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Exhaust);
+		var state = model.State.ToReactiveProperty();
+
+		model.UserFinishedMode();
+
+		Assert.AreEqual(CardHandlingMode.Regular, state.Value.Mode);
+	}
+
+	[Test]
+	public void ShouldPlayCardFromHand() {
+		var deck = new Card[] { new Card {
+			Name = "foo",
+			GoldCost = 1,
+			GoldGain = 5
+		}};
+		var model = new SceneModel(CardsState.NewState(deck).ShuffleCurrentDeck().DrawCardsToHand(1),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Regular);
+		var state = model.State.ToReactiveProperty();
+
+		model.PlayCard(deck[0]);
+
+		Assert.AreEqual(0, state.Value.Empire.Gold);
+		Assert.AreEqual(5, state.Value.Empire.AddGold);
+		Assert.AreEqual(0, state.Value.Cards.Hand.Count());
+		Assert.AreEqual(deck, state.Value.Cards.DiscardPile);
+		Assert.AreEqual(0, state.Value.Cards.CurrentDeck.Count());
+	}
+
+	[Test]
+	public void ShouldFailToPlayExpensiveCardFromHand() {
+		var deck = new Card[] { new Card {
+			Name = "foo",
+			GoldCost = 2,
+			GoldGain = 5
+		}};
+		var model = new SceneModel(CardsState.NewState(deck).ShuffleCurrentDeck().DrawCardsToHand(1),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Regular);
+		var state = model.State.ToReactiveProperty();
+
+		model.PlayCard(deck[0]);
+
+		Assert.AreEqual(1, state.Value.Empire.Gold);
+		Assert.AreEqual(0, state.Value.Empire.AddGold);
+		Assert.AreEqual(0, state.Value.Cards.DiscardPile.Count());
+		Assert.AreEqual(0, state.Value.Cards.CurrentDeck.Count());
+		Assert.AreEqual(deck, state.Value.Cards.Hand);
+	}
+
+	[Test]
+  public void ShouldDrawCardAndPayOneGold() {
+		var model = new SceneModel(CardsState.NewState(initialDeck).ShuffleCurrentDeck(),
+			new EmpireState(1, 0, 0, 0), CardHandlingMode.Regular);
+		var state = model.State.ToReactiveProperty();
+
+		model.TryDrawCard();
+
+		Assert.AreEqual(1, state.Value.Cards.Hand.Count());
+		Assert.AreEqual(new EmpireState(0, 0, 0, 0), state.Value.Empire);
+	}
+
+	[Test]
+	public void ShouldNotDrawCardIfNoGoldIsAvailable() {
+		var model = new SceneModel(CardsState.NewState(initialDeck).ShuffleCurrentDeck(),
+			new EmpireState(0, 0, 0, 0), CardHandlingMode.Regular);
+		var state = model.State.ToReactiveProperty();
+
+		model.TryDrawCard();
+
+		Assert.AreEqual(0, state.Value.Cards.Hand.Count());
+		Assert.AreEqual(new EmpireState(0, 0, 0, 0), state.Value.Empire);
+	}
+}
---
+++ CardGame/Assets/scripts/model/SceneModel.cs
@@ -0,0 +1,207 @@
+﻿// Copyright (c) 2018 Shachar Langbeheim. All rights reserved.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public enum CardHandlingMode { Regular, Event, Replace, Exhaust }
+
+public class SceneState {
+	public CardsState Cards { get; }
+	public EmpireState Empire { get; }
+	public CardHandlingMode Mode { get; }
+	public EventCard CurrentEvent { get; }
+
+	public SceneState(CardsState cards, 
+		EmpireState empire, 
+		CardHandlingMode mode,
+		EventCard currentEvent) {
+		Cards = cards;
+		Empire = empire;
+		Mode = mode;
+		CurrentEvent = currentEvent;
+	}
+}
+
+public interface ISceneModel {
+	IObservable<SceneState> State { get; }
+
+	bool PlayCard(Card card);
+	void UserFinishedMode();
+	void TryDrawCard();
+}
+
+public class SceneModel : ISceneModel {
+	private CardsState cards;
+	private EmpireState empireState;
+	private CardHandlingMode mode = CardHandlingMode.Regular;
+	private List<EventCard> events = EventCardsCollections.Cards().ToList();
+	private readonly List<Card> playedCards = new List<Card>();
+	private int cardsToHandle;
+	private ReplaySubject<SceneState> stateSubject = new ReplaySubject<SceneState>(1);
+
+	public SceneModel() : this(
+		CardsState.NewState(CardsCollection.Cards())
+			.ShuffleCurrentDeck()
+			.DrawCardsToHand(Constants.MAX_CARDS_IN_HAND),
+		new EmpireState(1, 1, 2, 0), 
+		CardHandlingMode.Regular) {}
+
+	public SceneModel(CardsState cards, EmpireState empireState, 
+		CardHandlingMode mode) {
+		this.cards = cards;
+		this.empireState = empireState;
+		this.mode = mode;
+		EventUtils.LogStartTurnEvent(eventCardName(), empireState.ToString(), cards.Hand);
+		sendCompletedState();
+	}
+
+	#region ISceneModel
+	public IObservable<SceneState> State => stateSubject;
+
+	public bool PlayCard(Card card) {
+		bool succeeded = false;
+		switch (mode) {
+			case CardHandlingMode.Regular:
+				succeeded = playRegularCard(card);
+				break;
+			case CardHandlingMode.Event:
+				succeeded = playEventCard(card);
+				break;
+			case CardHandlingMode.Replace:
+				succeeded = replaceCard(card);
+				break;
+			case CardHandlingMode.Exhaust:
+				succeeded = exhaustCard(card);
+				break;
+		}
+		sendCompletedState();
+		return succeeded;
+	}
+
+	private bool playRegularCard(Card card) {
+		if (!empireState.CanPlayCard(card)) {
+			Debug.Log($"Can't play {card.Name}");
+			return false;
+		}
+		empireState = empireState.PlayCard(card);
+		cards = cards.PlayCard(card);
+		playedCards.Add(card);
+		switchModeAccordingToCard(card);
+		return true;
+	}
+
+	private void switchModeAccordingToCard(Card card) {
+		if (card.NumberOfCardsToChooseToExhaust > 0) {
+			mode = CardHandlingMode.Exhaust;
+			cardsToHandle = card.NumberOfCardsToChooseToExhaust;
+		} else if (card.NumberOfCardsToChooseToReplace > 0) {
+			mode = CardHandlingMode.Replace;
+			cardsToHandle = card.NumberOfCardsToChooseToReplace;
+		}
+	}
+
+	private bool playEventCard(Card card) {
+		if (!empireState.CanPlayCard(card)) {
+			Debug.Log($"Can't play {card.Name}");
+			return false;
+		}
+		empireState = empireState.PlayCard(card);
+		empireState = empireState.NextTurnState();
+		EventUtils.LogEventCardPlayed(card, eventCardName(), empireState.ToString(), cards.Hand);
+		mode = CardHandlingMode.Regular;
+		startTurn();
+		return true;
+	}
+
+	private bool replaceCard(Card card) {
+		cards = cards
+			.DiscardCardFromHand(card)
+			.DrawCardsToHand(1);
+		changeModeIfNeeded();
+		return true;
+	}
+
+	private bool exhaustCard(Card card) {
+		cards = cards.ExhaustCardFromHand(card);
+		changeModeIfNeeded();
+		return true;
+	}
+
+	private void changeModeIfNeeded() {
+		cardsToHandle--;
+		if (cardsToHandle != 0) {
+			return;
+		}
+		mode = CardHandlingMode.Regular;
+	}
+
+	private void startTurn() {
+		drawNewHand();
+		empireState = empireState.NextTurnState();
+		EventUtils.LogStartTurnEvent(eventCardName(), empireState.ToString(), cards.Hand);
+		playedCards.Clear();
+	}
+
+	private void drawNewHand() {
+		var remainingCards = Math.Max(Constants.MAX_CARDS_IN_HAND - cards.CurrentDeck.Count(), 0);
+		cards = cards.DrawCardsToHand(Constants.MAX_CARDS_IN_HAND - remainingCards);
+		if (remainingCards > 0) {
+			cards = cards.ShuffleDiscardToDeck()
+				.DrawCardsToHand(remainingCards);
+		}
+	}
+
+	public void UserFinishedMode() {
+		switch (mode) {
+			case CardHandlingMode.Exhaust:
+			case CardHandlingMode.Replace:
+				mode = CardHandlingMode.Regular;
+				break;
+			case CardHandlingMode.Regular:
+				endTurn();
+				break;
+			default:
+				AssertUtils.UnreachableCode();
+				break;
+		}
+		sendCompletedState();
+	}
+
+	private void endTurn() { 
+		empireState = empireState.NextTurnState();
+		EventUtils.LogEndTurnEvent(playedCards, eventCardName(), empireState.ToString(), cards.Hand);
+		mode = CardHandlingMode.Event;
+		if (events.Count > 1) events.RemoveAt(0);
+		cards = cards.DiscardHand();
+	}
+
+	public void TryDrawCard() {
+		if (!canDrawCard()) {
+			return;
+		}
+
+		cards = cards.DrawCardsToHand(1);
+		empireState = empireState.ChangeGold(-1);
+		sendCompletedState();
+	}
+
+	private bool canDrawCard() {
+		return empireState.Gold > 0 &&
+			cards.Hand.Count() < Constants.MAX_CARDS_IN_HAND &&
+			cards.CurrentDeck.Count() > 0;
+	}
+
+	#endregion
+
+	private void sendCompletedState() {
+		stateSubject.OnNext(new SceneState(cards, empireState, mode, events.First()));
+	}
+
+	private string eventCardName() {
+		return events[0].Name;
+	}
+}
